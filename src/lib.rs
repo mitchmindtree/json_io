7a9430cb4a3c7c8b97e56c291e50a1434fb0eb68
mitchmindtree <mitchell.nordine@gmail.com>
2015-05-30 16:28:45 +1000

    Made load check for file with and without .json extension

--- src/lib.rs
+++ src/lib.rs
@@ -44,45 +44,37 @@ impl ::std::error::Error for Error {
 
 
 /// Construct a Library from a JSON file.
-/// The `Path` should be the absolute path of the file without the ".json" extension.
+/// json_io will first try and open the file with the path exactly as given.
+/// If the file isn't found, it will set the extension to .json and try again.
 pub fn load<T: Decodable>(path: &Path) -> Result<T, Error> {
-    let mut path = path.to_path_buf();
-    path.set_extension("json");
     let mut file = match File::open(&path) {
         Ok(file) => file,
-        Err(err) => return Err(Error::IO(err)),
+        Err(err) => {
+            match err.kind() {
+                std::io::ErrorKind::NotFound => {
+                    let mut path = path.to_path_buf();
+                    path.set_extension("json");
+                    try!(File::open(&path).map_err(|err| Error::IO(err)))
+                },
+                _ => return Err(Error::IO(err)),
+            }
+        },
     };
     let mut contents = Vec::new();
-    if let Err(err) = io::Read::read_to_end(&mut file, &mut contents) {
-        return Err(Error::IO(err));
-    }
-    let json_object = match json::Json::from_str(std::str::from_utf8(&contents[..]).unwrap()) {
-        Ok(json_object) => json_object,
-        Err(err) => return Err(Error::JsonParserError(err)),
-    };
+    try!(io::Read::read_to_end(&mut file, &mut contents).map_err(|err| Error::IO(err)));
+    let json_object = try!(json::Json::from_str(std::str::from_utf8(&contents[..]).unwrap())
+        .map_err(|err| Error::JsonParserError(err)));
     let mut decoder = json::Decoder::new(json_object);
-    match Decodable::decode(&mut decoder) {
-        Ok(t) => Ok(t),
-        Err(err) => Err(Error::JsonDecoderError(err)),
-    }
+    T::decode(&mut decoder).map_err(|err| Error::JsonDecoderError(err))
 }
 
 /// Save a Library to a JSON file.
-/// The `Path` should be the absolute path of the file without the ".json" extension.
+/// The file will be saved with the ".json" extension whether or not it was given with the Path.
 pub fn save<T: Encodable>(path: &Path, t: &T) -> Result<(), Error> {
     let mut path = path.to_path_buf();
     path.set_extension("json");
-    let json_string = match json::encode(&t) {
-        Ok(x) => x,
-        Err(err) => return Err(Error::JsonEncoderError(err)),
-    };
-    let mut file = match File::create(&path) {
-        Ok(file) => file,
-        Err(err) => return Err(Error::IO(err)),
-    };
-    match io::Write::write_all(&mut file, json_string.as_bytes()) {
-        Ok(()) => Ok(()),
-        Err(err) => Err(Error::IO(err)),
-    }
+    let json_string = try!(json::encode(&t).map_err(|err| Error::JsonEncoderError(err)));
+    let mut file = try!(File::create(&path).map_err(|err| Error::IO(err)));
+    io::Write::write_all(&mut file, json_string.as_bytes()).map_err(|err| Error::IO(err))
 }
 
